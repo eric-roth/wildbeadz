b996e962a8723f32aec9433703e3baef84acc363
ericroth2 <ericroth2@delta.edu>
2022-03-16 16:33:44 -0400

    add login

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,15 @@
 import { Typography, Button } from "@mui/material";
+import UserDataService from "../services/user.services";
 import React from "react";
 import { useUserAuth } from "../context/AuthContext";
 
 function Home() {
-  let { user, logOut } = useUserAuth();
-  const handleLogout = async () => {
-    try {
-      await logOut();
-    } catch (err) {
-      console.log(err.message);
-    }
-  };
+  let { user } = useUserAuth();
 
   return (
     <div>
       <Typography variant='h1'>Home</Typography>
       <Typography>{user && `Hello, ${user.email}`}</Typography>
-      <Button onClick={handleLogout}>Logout</Button>
     </div>
   );
 }
